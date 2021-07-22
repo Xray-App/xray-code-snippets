3ff2d638ca52fa31b3050ead52938efa4cd42cf6
Sergio Freire <sergio.freire@xpand-it.com>
2021-07-22 17:50:44 +0100

    read client_id and client_secret from env vars

--- use_cases/import_automation_results/js/import_results_robot_standard_axios_cloud.js
+++ use_cases/import_automation_results/js/import_results_robot_standard_axios_cloud.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var FormData = require('form-data');
 
 var xray_cloud_base_url = "https://xray.cloud.xpand-it.com/api/v2";
-var client_id = "215FFD69FE4644728C72182E00000000";
-var client_secret = "1c00f8f22f56a8684d7c18cd6147ce2787d95e4da9f3bfb0af8f02ec00000000";
+var client_id = process.env.CLIENT_ID || "215FFD69FE4644728C72182E00000000";
+var client_secret = process.env.CLIENT_SECRET || "1c00f8f22f56a8684d7c18cd6147ce2787d95e4da9f3bfb0af8f02ec00000000";
 
 var authenticate_url = xray_cloud_base_url + "/authenticate";
 
