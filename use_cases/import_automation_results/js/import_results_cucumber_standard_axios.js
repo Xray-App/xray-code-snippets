1ee53c9591ed8bcddf3321d81b7a5a0ea13d5ebd
Sergio Freire <sergio.freire@xpand-it.com>
2022-04-19 11:13:21 +0100

    add JS examples for importing cucumber reports using the standard endpoint

---
+++ use_cases/import_automation_results/js/import_results_cucumber_standard_axios.js
@@ -0,0 +1,23 @@
+var btoa = require('btoa');
+var axios = require('axios');
+var fs = require('fs');
+var FormData = require('form-data');
+
+var jira_base_url = "http://192.168.56.102";
+var personal_access_token = "OTE0ODc2NDE2NTgxOnrhigwOreFoyNIA9lXTZaOcgbNY";
+
+//var basicAuth = 'Basic ' + btoa(jira_username + ':' + jira_password);
+
+const report_content = fs.readFileSync("cucumber.json").toString();
+console.log(report_content);
+
+var endpoint_url = jira_base_url + "/rest/raven/2.0/import/execution/cucumber";
+axios.post(endpoint_url, report_content, {
+    //headers: { 'Authorization': basicAuth, "Content-Type": "application/json" }
+    headers: { 'Authorization': "Bearer " + personal_access_token, "Content-Type": "application/json" }
+}).then(function(response) {
+    console.log('success');
+    console.log(response.data.testExecIssue.key);
+}).catch(function(error) {
+    console.log('Error submiting results: ' + error);
+});
