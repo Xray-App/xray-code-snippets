babbb52c55a68200427936546c29947aa1662fc3
CMCunha <cristiano.cunha@gmail.com>
2023-08-17 11:54:35 +0100

    TestRail migration code and samples.

---
+++ use_cases/import_from_testrail/README.md
@@ -0,0 +1,41 @@
+
+# How to import test cases from TestRail into Xray
+
+## Background
+
+Here you can find some examples showcasing how you can export test cases from TestRail and import them into Xray using Test Case Importer.
+This repository supports the tutorial [Importing TestRail test cases using Test Case Importer](https://docs.getxray.app/display/XRAYCLOUD/Importing+TestRail+test+cases+using+Test+Case+Importer)
+
+[TestRail](https://www.testrail.com/) is a test management tool.
+
+We are using the export to XML capability of TestRail so you can also find one script (one for cloud and one for Server) that converts that XML file into a compatible CSV file, ready to be uploaded.
+
+## Contents
+
+In this repo you'll find a set of examples for cloud and server version of Xray on how to convert XML files exported from TestRail using a script present in the repository and uploading it to Xray. 
+
+In each directory you have (same for cloud and server):
+* cloud/Server
+    * One XML file that is the result of exporting from TestRail
+    * One CSV file that is the result of executing the script (testrail2Xray.py)  against the XML file
+    * One configuration Json file that you can use to configure the mappings in the Test Case Importer
+    * testrail2Xray.py - Scripts that will convert XML files to CSV files for cloud and server versions respectively. 
+
+### Script usage
+
+#### Cloud
+```Python
+python3 testrail2Xray.py -i comic_estore.xml -o comicEstore.csv
+```
+The output for cloud version creates preconditions and test sections (if they exist in TestRail) and link those to the tests in one import. It supports Manual, Automated and Exploratory tests.
+
+#### Server
+```Python
+python3 testrail2XrayServer.py -i comic_estore.xml -o comicEstore.csv
+```
+The output for server version only creates the tests into Xray and the Test Repo structure. Creation and association of preconditions must be done separately. It only supports Manual Tests.
+
+
+### Source-code
+
+- [Python](./python/)
---
+++ use_cases/import_from_testrail/server/testrail2Xray.py
@@ -0,0 +1,169 @@
+import sys, getopt
+import re
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+column = ["Issue ID","Issue Key","Test Type","Test Summary", "Test Priority", "Action","Data","Result","Test Repo", "Labels"]
+row = []
+
+CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+EMPTYSPACES = re.compile('\n|\r')
+QUOTES = re.compile('\&quot\;')
+
+def cleanTags(txt):
+    if txt:
+        cleanTxt = re.sub(QUOTES, '"', txt)
+        cleanTxt = re.sub(CLEANR, '', cleanTxt)
+        #cleanTxt = re.sub(EMPTYSPACES, ' ', cleanTxt)
+    else:
+        cleanTxt = ''
+
+    return cleanTxt
+
+def getPriorityValue(priorityName):
+    priority = 4
+
+    if priorityName == 'Critical':
+        priority = 1
+    elif priorityName == 'High':
+        priority = 2
+    elif priorityName == 'Medium':
+        priority = 3
+    elif priorityName == 'Low':
+        priority = 4
+    else:
+        priority = 3
+    return priority
+
+def getTestType(type):
+    testType = 'Manual'
+    validTypes = ['Manual', 'Exploratory', 'Automated']
+    if type is not None and type in validTypes:
+        if type == 'Automated':
+            testType = 'Generic'
+        else:
+            testType = type
+
+    return testType
+
+def appendRows(issueID='', issueKey='', testType=None, testSummary=None, testPriority=None, action=None, data=None, result=None, testRepo=None, labels=None):
+    row.append({"Issue ID": issueID,
+                "Issue Key": '',
+                "Test Type": getTestType(testType),
+                "Test Summary": cleanTags(testSummary.text) if testSummary is not None else '',
+                "Test Priority": getPriorityValue(testPriority.text) if testPriority is not None else '3',
+                "Action": cleanTags(action.text) if action is not None else '',
+                "Data": cleanTags(data.text) if data is not None else '',
+                "Result": cleanTags(result.text) if result is not None else '',
+                "Test Repo": testRepo if testRepo else '',
+                "Labels": labels.text if labels is not None else ''})
+
+def handleTestSections(root, issueID, outputfile, repoName):
+    if root.tag == 'suite':
+        testsections = root.findall('sections/section')
+    else:
+        testsections = root.findall('section')
+
+    baseRepoName= repoName
+    for testsection in testsections:
+        testRepoName = testsection.find('name').text
+        testRepoDescription = testsection.find('description')
+
+        if baseRepoName is not None and testRepoName is not None:
+            testRepoName = baseRepoName+'/'+testRepoName
+
+        cases = testsection.findall('cases/case')
+        for testcase in cases:
+            preconditionID = ''
+            id = testcase.find('id')
+            title = testcase.find('title')
+            template = testcase.find('template')
+            labels = testcase.find('type')
+            priority = testcase.find('priority')
+            estimate = testcase.find('estimate')
+
+            custom = testcase.find('custom')
+            if custom:
+                automation_type = custom.find('automation_type')
+                type = automation_type.find('value').text.strip() if automation_type is not None else 'Manual'
+                                   
+                steps = custom.find('steps')
+                steps_separated = custom.find('steps_separated')
+
+                if steps is not None:
+                    # Text field with steps
+                    expected = custom.find('expected')
+                    appendRows(issueID=issueID,testType=type,testSummary=title,testPriority=priority,action=steps,result=expected, testRepo=testRepoName, labels=labels)
+                    issueID = issueID+1  
+                elif steps_separated is not None:
+                    # Steps in different cells
+                    first_step = True
+                    hasSteps = False
+                    for step in steps_separated:
+                        index = step.find('index')
+                        content = step.find('content')
+                        expected = step.find('expected')
+                        additional_info = step.find('additional_info')
+                        hasSteps = True
+                        if first_step:
+                            appendRows(issueID=issueID,testType=type,testSummary=title,testPriority=priority,action=content,result=expected,testRepo=testRepoName, data=additional_info, labels=labels)
+                            first_step = False
+                        else:
+                            appendRows(issueID=issueID,testType=type, action=content,result=expected, data=additional_info)
+                    if not hasSteps:
+                        appendRows(issueID=issueID,testType=type,testSummary=title,testPriority=priority, testRepo=testRepoName, data=additional_info)
+                    issueID = issueID+1  
+                else:
+                    #ignore all other types 
+                    continue
+            else:
+                # Empty line
+                #appendRows(issueID=issueID,testType=labels.text,testSummary=title,testPriority=priority, testRepo=testRepoName, labels=labels)
+                #issueID = issueID+1  
+                continue
+        
+        innerSection = testsection.find('sections')
+        if innerSection is not None:
+            issueID = handleTestSections(root=innerSection, issueID=issueID, outputfile=outputfile, repoName=testRepoName)
+
+        df = pd.DataFrame(row, columns=column)  
+        df.set_index("Issue ID", inplace=True)
+        df.to_csv(outputfile)
+    return issueID
+
+
+def parseTestrail2XrayData(inputfile, outputfile):
+    # Parsing XML file
+    xmlParse = ET.parse(inputfile)
+    root = xmlParse.getroot()
+    issueID = 1
+
+    handleTestSections(root=root, issueID=issueID, outputfile=outputfile, repoName=None)
+    
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+
+   try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        for opt, arg in opts:
+            if opt == '-h':
+                print ('testrail2Xray.py -i <XML_inputfile> -o <CSV_outputfile>')
+                sys.exit()
+            elif opt in ("-i", "--ifile"):
+                inputfile = arg
+            elif opt in ("-o", "--ofile"):
+                outputfile = arg
+   except Exception as err:
+       print ("An exception occurred:", err)
+
+   #inputfile='/Users/cristianocunha/Documents/Projects/tutorials/xray-code-snippets/use_cases/import_from_testrail/comic_estore.xml'
+   #outputfile='/Users/cristianocunha/Documents/Projects/tutorials/xray-code-snippets/use_cases/import_from_testrail/comicEStore.csv'
+   if not inputfile or not outputfile:
+    print ('One of the input parameters is missing, please use: testrail2Xray.py -i <XML_inputfile> -o <CSV_outputfile>')
+    sys.exit()
+
+   parseTestrail2XrayData(inputfile, outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
