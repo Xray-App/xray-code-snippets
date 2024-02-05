cf010dadc6e45c51493d4760b18d78105e6ef6ad
CMCunha <cristiano.cunha@gmail.com>
2024-02-05 12:43:17 +0000

    Adding Qase migration scripts and examples.

---
+++ use_cases/import_from_qase/README.md
@@ -0,0 +1,42 @@
+
+# How to import test cases from Qase into Xray
+
+## Background
+
+Here you can find some examples showcasing how you can export test cases from Qase and import them into Xray using Test Case Importer.
+This repository supports the tutorial [Importing Qase test cases using Test Case Importer](https://docs.getxray.app/display/XRAYCLOUD/Importing+Qase+test+cases+using+Test+Case+Importer)
+
+[Qase](https://qase.io/) is a test management tool.
+
+We are using the export to CSV capability of Qase and have produced one script (one for cloud and one for Server) that converts that CSV file into a compatible CSV file, ready to be uploaded.
+
+## Contents
+
+In this repo you'll find a set of examples for cloud and server version of Xray on how to convert CSV files exported from Qase using a script present in the repository and uploading it to Xray. 
+
+In each directory you have (same for cloud and server):
+* cloud/Server
+    * One CSV file that is the result of exporting from Qase
+    * One CSV file that is the result of executing the script (qase2Xray.py)  against the CSV file
+    * One configuration Json (Cloud) or txt (Server) file that you can use to configure the mappings in the Test Case Importer
+    * qase2Xray.py - Scripts that will convert CSV files to CSV files for cloud and server versions respectively. 
+
+### Script usage
+
+#### Cloud
+```Python
+python3 qase2Xray.py -i qase_export.xml -o qase_xray_results.csv
+```
+The output for cloud version creates preconditions and link those to the tests in one import. It supports Scripted and BDD test types.
+
+#### Server
+```Python
+python3 qase2Xray.py -i qase_export.xml -o qase_xray_results.csv
+```
+
+The output for server version only creates the tests into Xray. Creation and association of preconditions must be done separately. It only supports Manual Tests.
+
+
+### Source-code
+
+- [Python3](https://www.python.org/downloads/release/python-3115/)
---
+++ use_cases/import_from_qase/server/qase2Xray.py
@@ -0,0 +1,152 @@
+import sys, getopt
+import re
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+column = ["Issue ID","Issue Key","Test Type","Test Summary", "Test Priority", "Action","Data","Result","Issue Type", "Precondition", "Precondition Type", "Description", "Unstructured Definition", "Gherkin Definition", "Test Repo"]
+row = []
+
+CLEANR = re.compile(r'<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+EMPTYSPACES = re.compile(r'\n|\r')
+QUOTES = re.compile(r'\&quot\;')
+CLEANSTARTINGNUM = re.compile(r'\d\.')
+
+def cleanTags(txt):
+    if txt:
+        cleanTxt = re.sub(QUOTES, '"', txt)
+        cleanTxt = re.sub(CLEANR, '', cleanTxt)
+        cleanTxt = re.sub(EMPTYSPACES, ' ', cleanTxt)
+    else:
+        cleanTxt = ''
+
+    return cleanTxt
+
+def getPriorityValue(priorityName):
+    priority = 4
+
+    if priorityName == 'Urgent':
+        priority = 1
+    elif priorityName == 'High':
+        priority = 2
+    elif priorityName == 'Medium':
+        priority = 3
+    elif priorityName == 'Low':
+        priority = 4
+    else:
+        priority = 3
+    return priority
+
+def getTestType(automation, stepsType):
+    testType = 'Manual'
+    #validTypes = ['Functional', 'Smoke', 'regression', 'performance', 'security', 'usability','acceptance','compatability','integration','exploratory']
+    if automation is not None and stepsType is not None:
+        if stepsType == 'gherkin':
+            testType = 'Cucumber'
+        elif automation == 'automated':
+            testType = 'Generic'
+        else:
+            testType = 'Manual'
+
+    return testType
+
+def appendRows(issueID='', issueKey='', stepsType=None, testType=None, testSummary=None, testPriority=None, action=None, data=None, result=None, issueType=None, description=None, precondition=None, unstrusturedDefinition=None, gherkinDefinition=None, testRepo=None):
+    noActionTypes = ['Scenario', 'Automation']
+    row.append({"Issue ID": issueID,
+                "Issue Key": '',
+                "Test Type": getTestType(testType, stepsType),
+                "Test Summary": testSummary if testSummary is not None else '',
+                "Test Priority": getPriorityValue(testPriority) if testPriority is not None else '3',
+                "Action": action if action is not None and testType not in noActionTypes else '',
+                "Data": data if data is not None and testType not in noActionTypes else '',
+                "Result": result if result is not None and testType not in noActionTypes else '',
+                "Precondition": precondition if precondition is not None and precondition != 0 else '',
+                "Precondition Type": '',
+                "Unstructured Definition": unstrusturedDefinition if unstrusturedDefinition is not None and testType == 'ApiTest' else '',
+                "Issue Type": 'Test',
+                "Description": description if description is not None else '',
+                "Gherkin Definition": gherkinDefinition if gherkinDefinition is not None and stepsType == 'gherkin' else '',
+                "Test Repo": testRepo})
+
+    
+def parseqase2XrayData(inputfile, outputfile):
+    df = pd.read_csv(inputfile)
+    
+    issueID = 1
+    testID = ''
+    suites = []
+    suites.insert(0, 'EMPTY')
+    for index, xls_row in df.iterrows():
+        preconditionID = 0
+        testID = xls_row['v2.id']
+        stepsType = xls_row['steps_type'] #classic or gherkin
+        suiteID = xls_row['suite_id']
+        automation = xls_row['automation'] # automated or is-not-automated
+        suite = xls_row['suite']
+
+        if pd.isna(testID):
+            #Process suites
+            suiteParentID = xls_row['suite_parent_id']
+            if not pd.isna(suiteParentID):
+                suites.insert(int(suiteID), suites[int(suiteParentID)] + '/' + suite)
+            else:                
+                suites.insert(int(suiteID), suite)
+        else:
+            #Process tests
+            tags = xls_row['tags']
+            priority = xls_row['priority']
+            automation = xls_row['automation'] #automated or is-not-automated
+            steps_actions = xls_row['steps_actions']
+            steps_result = xls_row['steps_result']
+            steps_data = xls_row['steps_data']
+            suite_id = xls_row['suite_id']
+            suite_parent_id = xls_row['suite_parent_id']
+
+            if not pd.isna(steps_actions) and not stepsType == 'gherkin':
+                splitActions = steps_actions.splitlines()
+                splitDatas = steps_data.splitlines()
+                splitResults = steps_result.splitlines()
+                index = 0
+
+                for action in splitActions:
+                    if index == 0:
+                        appendRows(issueID=issueID, stepsType=stepsType, testType=automation, testSummary=xls_row['title'], testPriority=priority, action=re.sub(CLEANSTARTINGNUM, '', splitActions[index]), result=re.sub(CLEANSTARTINGNUM, '', splitResults[index]), data=re.sub(CLEANSTARTINGNUM, '', splitDatas[index]), description=xls_row['description'], precondition=preconditionID, unstrusturedDefinition='', gherkinDefinition='', testRepo=suites[int(suite_id)])
+                        index = index+1
+                    else:
+                        appendRows(issueID=issueID, stepsType=stepsType, testType=automation, testSummary='', testPriority=priority,action=re.sub(CLEANSTARTINGNUM, '', splitActions[index]),result=re.sub(CLEANSTARTINGNUM, '', splitResults[index]), data=re.sub(CLEANSTARTINGNUM, '', splitDatas[index]), unstrusturedDefinition='', gherkinDefinition='')
+                        index = index+1
+                    
+                issueID=issueID+1
+
+        df = pd.DataFrame(row, columns=column)  
+        df.set_index("Issue ID", inplace=True)
+        df.to_csv(outputfile) 
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+
+   try:
+        opts, args = getopt.getopt(argv,"hi:o:e:",["ifile=","ofile=","efile="])
+        for opt, arg in opts:
+            if opt == '-h':
+                print ('qase2Xray.py -i <CSV_inputfile> -o <CSV_outputfile>')
+                sys.exit()
+            elif opt in ("-i", "--ifile"):
+                inputfile = arg
+            elif opt in ("-o", "--ofile"):
+                outputfile = arg
+
+   except Exception as err:
+       print ("An exception occurred:", err)
+
+   #inputfile='/Users/cristianocunha/Documents/Projects/tutorials/xray-code-snippets/use_cases/import_from_qase/server/qase_export.csv'
+   #outputfile='/Users/cristianocunha/Documents/Projects/tutorials/xray-code-snippets/use_cases/import_from_qase/server/qase_xray_results.csv'
+
+   if not inputfile or not outputfile:
+        print ('One of the input parameters is missing, please use: qase2Xray.py -i <CSV_inputfile> -o <CSV_outputfile>')
+        sys.exit()
+
+   parseqase2XrayData(inputfile, outputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
